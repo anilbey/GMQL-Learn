bff0f69cd140e6cfbb8bc7001c1cc45b1fef4238
anilbey <tuncel.manil@gmail.com>
2017-05-26 12:41:53 +0200

    Refactoring

--- data_model.py
+++ data_model.py
@@ -1,9 +1,3 @@
-# contains the datamodel class with metadata and region data structures
-# provides data manipulation methods
-# keeps track of the index
-# creates compact views
-
-
 import pandas as pd
 from parser.parser import Parser
 import numpy as np
@@ -15,12 +9,32 @@ class DataModel:
         self.meta = None
         return
 
-    def load(self, path, regs=[], meta=[], values=[]):
-        p = Parser(path, regs, meta, values)
-        self.data = p.data
-        self.meta = p.meta
+    def load(self, _path, regs=['chr', 'left', 'right', 'strand'], meta=[], values=[]):
+        """Parses and loads the data into instance attributes.
+
+        Args:
+            _path: The path to the dataset on the filesystem.
+            regs: the regions that are to be analyzed.
+            meta: the meta-data that are to be analyzed.
+            values: the values that are to be selected.
+
+        """
+        p = Parser(_path)
+        self.meta = p.parse_meta(meta)
+        self.data = p.parse_data(regs, values)
+
+    @classmethod
+    def from_multi_reference(cls):
+        pass
 
     def combine_meta(self, selected_meta):
+        """Sets one axis of the 2D multi-indexed dataframe
+            index to the selected meta data.
+
+        Args:
+            selected_meta: The list of the metadata users want to index with.
+
+        """
         meta_names = list(selected_meta)
         meta_names.append('sample')
         meta_index = []
@@ -30,4 +44,24 @@ class DataModel:
         meta_index = np.asarray(meta_index)
         multi_meta_index = pd.MultiIndex.from_arrays(meta_index, names=selected_meta)
         self.data.index = multi_meta_index
+
+    def to_matrix(self, values, multi_index):
+        """Creates a 2D multi-indexed matrix representation of the data.
+            This representation allows the data to be sent to the machine learning algorithms.
+
+        Args:
+            values: The value or values that are going to fill the matrix.
+            multi_index: The index to one axis of the matrix.
+
+        """
+        if isinstance(values, list):
+            for v in values:
+                self.data[v] = self.data[v].map(float)
+        else:
+            self.data[values] = self.data[values].map(float)
+        print("started pivoting")
+        self.data = pd.pivot_table(self.data,
+                                   values=values, columns=multi_index, index=['sample'],
+                                   fill_value=0)
+        print("end of pivoting")
 
--- parser/parser.py
+++ parser/parser.py
@@ -5,38 +5,25 @@ General purpose parser for the (tab separated) output of the MAP operations of G
 """
 
 import pandas as pd
-import numpy as np
 import os
 import xml.etree.ElementTree
 
 
 class Parser:
-    def __init__(self, path, selected_region_data, selected_meta, selected_values):
-        # complete constructor
-        # reads
-        self.path = path
-        self.data = None
-        self.meta = None
-        self.schema = self._get_file("schema")
 
-        # copy the list since it is to be modified
-        regions = list(selected_region_data)
-        self.parse_meta(self.path, selected_meta)
-        if (type(selected_values) is list):
-            regions.extend(selected_values)
-        else:
-            regions.append(selected_values)
-        self.parse_data(self.path, regions, selected_values)
-        self.to_matrix(selected_values, selected_region_data)
+    def __init__(self, path):
+        self.path = path
+        self.schema = self._get_file("schema", path)
         return
 
-
-    def get_sample_id(self, path):
+    @staticmethod
+    def get_sample_id(path):
         sp = path.split('/')
         file_name = sp[-1]
         return file_name.split('.')[0]
 
-    def _get_files(self, extension, path):
+    @staticmethod
+    def _get_files(extension, path):
         # retrieves the files sharing the same extension
         files = []
         for file in os.listdir(path):
@@ -44,13 +31,14 @@ class Parser:
                 files.append(os.path.join(path, file))
         return sorted(files)
 
-    def _get_file(self, extension):
-        for file in os.listdir(self.path):
+    @staticmethod
+    def _get_file(extension, path):
+        for file in os.listdir(path):
             if file.endswith(extension):
-                return os.path.join(self.path, file)
+                return os.path.join(path, file)
 
-
-    def parse_schema(self, schema_file):
+    @staticmethod
+    def parse_schema(schema_file):
         # parses the schema and returns its columns
         e = xml.etree.ElementTree.parse(schema_file)
         root = e.getroot()
@@ -59,7 +47,8 @@ class Parser:
             cols.append(elem.text)
         return cols
 
-    def _get_sample_name(self, path):
+    @staticmethod
+    def _get_sample_name(path):
         sp = path.split('/')
         file_name = sp[-1]
         return file_name.split('.')[0]
@@ -76,61 +65,55 @@ class Parser:
         df = pd.DataFrame(data=data, index=columns)
         df = df.T
         sample = self._get_sample_name(fname)
-        if selected_meta_data:
+        if selected_meta_data:  # if null then keep all the columns
             df = df[selected_meta_data]
         df['sample'] = sample
         return df
 
-    def parse_meta(self, path, selected_meta_data):
+    def parse_meta(self, selected_meta_data):
         # reads all meta data files
-        files = self._get_files("meta", path)
+        files = self._get_files("meta", self.path)
         df = pd.DataFrame()
         for f in files:
             data = self.parse_single_meta(f, selected_meta_data)
             if data is not None:
                 df = pd.concat([df, data], axis=0)
         df.index = df['sample']
-        self.meta = df
+        return df
 
     def parse_single_data(self, path, cols, selected_region_data, selected_values):
         # reads a sample file
         df = pd.read_table(path, engine='c', sep="\t", lineterminator="\n")
         df.columns = cols  # column names from schema
         df = df[selected_region_data]
-        if (type(selected_values) is list):
+        if type(selected_values) is list:
             df_2 = pd.DataFrame(dtype=float)
             for value in selected_values:
                 df_3 = df.loc[df[value] != 0]
-                df_2 = pd.concat([df_2,df_3], axis=0)
+                df_2 = pd.concat([df_2, df_3], axis=0)
             df = df_2
         else:
             df = df.loc[df[selected_values] != 0]
         sample = self._get_sample_name(path)
         df['sample'] = sample
-        print(sample,end=' ')
+        print(sample, end=' ')
         return df
 
-    def parse_data(self, path, selected_region_data, selected_values):
+    def parse_data(self, selected_region_data, selected_values):
         # reads all sample files
-        files = self._get_files("gdm", path)
+        regions = list(selected_region_data)
+        if type(selected_values) is list:
+            regions.extend(selected_values)
+        else:
+            regions.append(selected_values)
+
+        files = self._get_files("gdm", self.path)
         df = pd.DataFrame(dtype=float)
 
         cols = self.parse_schema(self.schema)
         for f in files:
-            data = self.parse_single_data(f, cols, selected_region_data, selected_values)
+            data = self.parse_single_data(f, cols, regions, selected_values)
             if data is not None:
                 df = pd.concat([data, df], axis=0)
-        self.data = df
+        return df
 
-    def to_matrix(self, value, multi_index):
-        # creates a matrix dataframe
-        if isinstance(value, list):
-            for v in value:
-                self.data[v] = self.data[v].map(float)
-        else:
-            self.data[value] = self.data[value].map(float) # issue: does not map for multiple values
-        print("started pivoting")
-        self.data = pd.pivot_table(self.data,
-                                values=value, columns=multi_index, index=['sample'],
-                                fill_value=0)
-        print("end of pivoting")
